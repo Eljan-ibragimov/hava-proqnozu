fbd1371c03e4f176ec5be1e36c87c3e0772d9da4
eljan-ibragimov <elcan.ibrahimov85@gmail.com>
2022-10-23 09:26:06 -0700

    weather forecast

--- script.js
+++ script.js
@@ -1,31 +1,106 @@
 const url = 'https://api.openweathermap.org/data/2.5/'
 const key = '8fbaae7c8525ec41a1f9e6fe34e48710'
+const searchBar = document.getElementById('searchBar')
 
 const setQuery = (e) =>{
     if(e.keyCode == '13')
+
     getResult(searchBar.value)
+    
+
 }
 
-const getResult = (cityName) =>{
-    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=az`
-    fetch(query)
-    .then(weather =>{
-        return weather.json()
-    })
-  .then(displayResult)
+
+
+function getResult(cityName){
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=az`
+  fetch(query)
+  .then(weather =>{
+
+    if(!weather.ok) 
+  
+    throw new Error("Bölgəni düzgün daxil edin")
+    return weather.json()
+    
+  })
+.then(displayResult)
+.catch(err=>renderError(err))
+
 }
-const displayResult = (result) =>{
-  let city = document.querySelector('.city')
-  city.innerText = `${result.name},${result.sys.country}`
 
-  let temp = document.querySelector('.temp')
-  temp.innerText = `${Math.round(result.main.temp)}C°`
+// const getResult = (cityName) =>{
+//     let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=az`
+//     fetch(query)
+//     .then(weather =>{
 
-  let desc = document.querySelector('.desc')
-  desc.innerText = result.weather[0].description
+//       if(!weather.ok) 
+    
+//       throw new Error("Bölgəni düzgün daxil edin")
+//       return weather.json()
+      
+//     })
+//   .then(displayResult)
+//   .catch(err=>renderError(err))
 
-  let minmax = document.querySelector('.minmax')
-  minmax.innerText = `${Math.round(result.main.temp_min)}C°/ ${Math.round(result.main.temp_max)}C°`
+// }
+// const displayResult = (result) =>{
+//     // console.log(result)
+//   let city = document.querySelector('.city')
+//   city.innerText = `${result.name},${result.sys.country}`
+
+//   let temp = document.querySelector('.temp')
+//   temp.innerText = `${Math.round(result.main.temp)}C°`
+
+//   let desc = document.querySelector('.desc')
+//   desc.innerText =`${result.weather[0].description}
+//   Küləyin sürəti:${Math.round(result.wind.speed)}m/san`
+
+//   let minmax = document.querySelector('.minmax')
+//   minmax.innerText = `${Math.round(result.main.temp_min)}C°/ ${Math.round(result.main.temp_max)}C°`
+// }
+
+
+function displayResult (result){
+  console.log(result)
+let city = document.querySelector('.city')
+city.innerText = `${result.name},${result.sys.country}`
+
+let temp = document.querySelector('.temp')
+temp.innerText = `${Math.round(result.main.temp)}C°`
+
+let desc = document.querySelector('.desc')
+desc.innerText =`${result.weather[0].description}
+Küləyin sürəti:${Math.round(result.wind.speed)}m/san`
+
+let minmax = document.querySelector('.minmax')
+minmax.innerText = `${Math.round(result.main.temp_min)}C°/ ${Math.round(result.main.temp_max)}C°`
 }
-const searchBar = document.getElementById('searchBar')
+
+
+
+
+function renderError(err){
+  const html = `
+  <div class = "alert">
+  ${err.message}
+  </div>`;
+  document.querySelector("#error").innerHTML = html;
+
+}
 searchBar.addEventListener('keypress',setQuery)
+
+document.addEventListener('DOMContentLoaded', loaded)
+ function loaded(){
+  let query = `${url}weather?q=Bakı&appid=${key}&units=metric&lang=az`
+  fetch(query)
+  .then(weather =>{
+
+    if(!weather.ok) 
+  
+    throw new Error("Bölgəni düzgün daxil edin")
+    return weather.json()
+    
+  })
+.then(displayResult)
+  
+ }
